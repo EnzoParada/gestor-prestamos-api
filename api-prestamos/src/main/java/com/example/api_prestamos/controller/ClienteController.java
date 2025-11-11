5803e5a0644674571016642343fc3df2d3cef375
EnzoParada <paradaenzo26@outlook.com>
2025-11-11 03:15:09 -0300

    Implementacion de documentacion interactiva

--- api-prestamos/src/main/java/com/example/api_prestamos/controller/ClienteController.java
+++ api-prestamos/src/main/java/com/example/api_prestamos/controller/ClienteController.java
@@ -2,6 +2,9 @@ package com.example.api_prestamos.controller;
 
 import com.example.api_prestamos.model.Cliente;
 import com.example.api_prestamos.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +14,59 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Tag(name = "Clientes", description = "Gestion de clientes")
 @RequiredArgsConstructor
 public class ClienteController {
     private final ClienteService clienteService;
 
+    @Operation(
+            summary = "Crear un nuevo cliente.",
+            description = "Crea un nuevo cliente asignandole un id.",
+            responses = {
+                @ApiResponse(responseCode = "201", description = "Cliente creado exitosamente."),
+                @ApiResponse(responseCode = "400", description = "Datos de cliente inválidos.")
+    }
+    )
     @PostMapping
     public ResponseEntity<Cliente> crearCliete(@RequestBody Cliente cliente){
         return new ResponseEntity<>(clienteService.crearCliente(cliente), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Obtener todos los clientes.",
+            description = "Devuelve una lista de todos los clientes registrados en el sistema.",
+            responses = {
+                  @ApiResponse (responseCode = "200", description = "Lista de clientes devuelta (puede estar vacía)")
+            }
+            )
     @GetMapping
     public ResponseEntity<List<Cliente>> obtenerTodosLosCLientes(){
         return  new ResponseEntity<>(clienteService.obtenerTodosLosClientes(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Obtener clientes por id",
+            description ="Busca y devuelve un cliente específico usando su identificador único.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Cliente encontrado"),
+                    @ApiResponse(responseCode = "404", description = "Cliente no encontrado con ese ID")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> obtenerClientePorId(@PathVariable Long id){
         return  clienteService.obtenerClientePorId(id)
                 .map(cliente -> new ResponseEntity<>(cliente, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
+    @Operation(
+            summary = "Actualizar un cliente existente",
+            description = "Actualiza los detalles de un cliente existente usando su ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Cliente actualizado exitosamente"),
+                    @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos"),
+                    @ApiResponse(responseCode = "404", description = "Cliente no encontrado con ese ID")
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> actualizarCliente(
             @PathVariable Long id,
@@ -42,6 +77,14 @@ public class ClienteController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(
+            summary = "Eliminar un cliente",
+            description = "Elimina permanentemente un cliente del sistema usando su ID.",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Cliente eliminado exitosamente (No Content)"),
+                    @ApiResponse(responseCode = "404", description = "Cliente no encontrado con ese ID")
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> bajaCliente(@PathVariable Long id){
         clienteService.eliminarCliente(id);
--- api-prestamos/src/main/java/com/example/api_prestamos/controller/PrestamoController.java
+++ api-prestamos/src/main/java/com/example/api_prestamos/controller/PrestamoController.java
@@ -2,6 +2,9 @@ package com.example.api_prestamos.controller;
 
 import com.example.api_prestamos.model.Prestamo;
 import com.example.api_prestamos.service.PrestamoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +14,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Préstamos", description = "Gestión de las solicitudes y estados de los préstamos.")
 @RequiredArgsConstructor
 public class PrestamoController {
     private final PrestamoService prestamoService;
 
+    @Operation(
+            summary = "Crear un nuevo préstamo para un cliente",
+            description = "Asocia y registra un nuevo préstamo a un cliente específico usando su ID.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Préstamo creado exitosamente"),
+                    @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos"),
+                    @ApiResponse(responseCode = "404", description = "Cliente no encontrado")
+            }
+    )
     @PostMapping("/clientes/{clienteId}/prestamos")
     public ResponseEntity<Prestamo> crearPrestamo(
             @PathVariable Long clienteId,
@@ -22,7 +35,14 @@ public class PrestamoController {
         Prestamo nuevoPrestamo = prestamoService.crearPrestamo(clienteId, prestamo);
         return  new ResponseEntity<>(nuevoPrestamo, HttpStatus.CREATED);
     }
-
+    @Operation(
+            summary = "Obtener préstamos por ID de cliente",
+            description = "Devuelve una lista de todos los préstamos asociados a un cliente específico.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista de préstamos devuelta (puede estar vacía)"),
+                    @ApiResponse(responseCode = "404", description = "Cliente no encontrado")
+            }
+    )
     @GetMapping("/clientes/{clienteId}/prestamos")
     public ResponseEntity<List<Prestamo>> obtenerPrestamosPorCliente(@PathVariable Long clienteId){
         List<Prestamo> prestamos = prestamoService.obtenerPrestamosPorClientes(clienteId);
